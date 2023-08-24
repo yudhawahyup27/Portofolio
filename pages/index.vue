0684791dbb10da3e21ed787a8b49535f897bcf1f
Nairobi <Nairobi@192.168.1.23>
2023-08-24 10:00:41 +0700

    upload

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- Hero Slide -->
+    <div  id="hero" class="hero grid  grid-cols-3 flex flex-wrap p-2  shadow shadow-md  place-items-center">
+ <div class="text1  ">
+<h1 class="text-indigo-500 font-bold text-center md:text-left my-2 md:text-xl">Mobile Developer</h1>
+<span class="text-xs text-justify  hidden md:block">Seorang programmer junior mobile developer yang mahir dalam Flutter dan Kotlin, berfokus pada pembuatan aplikasi lintas platform dan Android. Saya berdedikasi dalam mempelajari dan mengimplementasikan solusi kreatif dalam pengembangan aplikasi. </span>
+ </div>
+ <div class="gambar">
+  <img class="w-120 " src="../public/heroimg.png" alt=""></div>
+ <div class=" ">
+   <h1 class="text-indigo-500 font-bold  text-center md:text-left my-2 md:text-xl">Front End Developer</h1>
+   <span class="text-xs text-justify hidden md:block">
+  Seorang programmer junior frontend developer yang menguasai bahasa Vue/Nuxt, React, Bootstrap, Tailwind, Svelte, Angular, dan Ember. Berdedikasi untuk menciptakan antarmuka menarik dan siap belajar lebih dalam untuk pengembangan tampilan web yang dinamis.</span>
+  </div>
+  <!-- Mobile -->
+  <!-- <div class="mobile  md:hidden block grid grid-cols-2 mx-2 my-2">
+     <div class="text1 visible md:invisible">
+    <h1 class="text-indigo-500 font-bold md:text-xl">Mobile Developer</h1>
+ <span class="text-xs text-justify">Seorang programmer junior mobile developer yang mahir dalam Flutter dan Kotlin, berfokus pada pembuatan aplikasi lintas platform dan Android. Saya berdedikasi dalam mempelajari dan mengimplementasikan solusi kreatif dalam pengembangan aplikasi. </span>
+     </div>
+     <div class="text1 visible md:invisible">
+    <h1 class="text-indigo-500 font-bold md:text-xl">Front End Developer</h1>
+       <span class="text-xs text-justify">Seorang programmer junior frontend developer yang menguasai bahasa Vue/Nuxt, React, Bootstrap, Tailwind, Svelte, Angular, dan Ember. Berdedikasi untuk menciptakan antarmuka menarik dan siap belajar lebih dalam untuk pengembangan tampilan web yang dinamis.</span>
+     </div>
+  </div> -->
+</div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'landing'
+
+}
+
+</script>
+
+<style>
+
+</style>
